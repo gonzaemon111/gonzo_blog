90dfc89041ea72d95c09450f0b11e62f753899ea
GokiSakuraba <goki14914@icloud.com>
2018-11-28 00:46:45 +0900

    add config.js in favicon

--- app/.vuepress/config.js
+++ app/.vuepress/config.js
@@ -1,5 +1,8 @@
 // .vuepress/config.js
 module.exports = {
     title: 'Gonzoemon\'sブログ',
-    description: 'https://github.com/gonzaemon111'
+    description: 'https://github.com/gonzaemon111',
+    head: [
+      ['link', { rel: 'icon', type: 'image/png', href: '/img/favicon.png' }]
+    ]
 }

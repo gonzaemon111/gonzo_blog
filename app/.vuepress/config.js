fe3b4438da4f3671774c992ea6be19f7e620c08f
GokiSakuraba <goki14914@icloud.com>
2018-11-29 01:33:33 +0900

    add config.js

--- app/.vuepress/config.js
+++ app/.vuepress/config.js
@@ -2,7 +2,46 @@
 module.exports = {
     title: 'Gonzoemon\'sブログ',
     description: 'https://github.com/gonzaemon111',
+    base: '/',
+    serviceWorker: true,
     head: [
       ['link', { rel: 'icon', type: 'image/png', href: '/img/favicon.png' }]
-    ]
+    ],
+    locales: {
+      '/': {
+        lang: 'ja'
+      }
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    markdown: {
+      lineNumbers: true,
+      linkify: true
+    },
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'About', link: '/about.html' },
+        { text: 'GitHub', link: 'https://github.com/rubytomato' }
+      ],
+      sidebarDepth: 2,
+      sidebar: 'auto',
+      lastUpdated: 'Last Updated'
+      // sidebar: {
+      //   // fallback
+      //   '/': [
+      //     '',        /* / */
+      //     'about'    /* /about.html */
+      //   ]
+      // }
+    },
+    configureWebpack: {
+      resolve: {
+        alias: {
+          '@alias': 'public/img'
+        }
+      }
+    }
 }
